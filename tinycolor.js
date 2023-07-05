d280e548e134597f2ee6b1b528bcdfdee52823e5
Adam <46270379+elchefe211@users.noreply.github.com>
2023-07-05 19:17:33 +0200

    Add files via upload

--- tinycolor.js
+++ tinycolor.js
@@ -255,7 +255,6 @@
     function rgbToHex(r, g, b, allow3Char) {
       var hex = [pad2(Math.round(r).toString(16)), pad2(Math.round(g).toString(16)), pad2(Math.round(b).toString(16))];
   
-      // Return a 3 character hex if possible
       if (allow3Char && hex[0].charAt(0) == hex[0].charAt(1) && hex[1].charAt(0) == hex[1].charAt(1) && hex[2].charAt(0) == hex[2].charAt(1)) {
         return hex[0].charAt(0) + hex[1].charAt(0) + hex[2].charAt(0);
       }
@@ -265,7 +264,6 @@
     function rgbaToHex(r, g, b, a, allow4Char) {
       var hex = [pad2(Math.round(r).toString(16)), pad2(Math.round(g).toString(16)), pad2(Math.round(b).toString(16)), pad2(convertDecimalToHex(a))];
   
-      // Return a 4 character hex if possible
       if (allow4Char && hex[0].charAt(0) == hex[0].charAt(1) && hex[1].charAt(0) == hex[1].charAt(1) && hex[2].charAt(0) == hex[2].charAt(1) && hex[3].charAt(0) == hex[3].charAt(1)) {
         return hex[0].charAt(0) + hex[1].charAt(0) + hex[2].charAt(0) + hex[3].charAt(0);
       }
